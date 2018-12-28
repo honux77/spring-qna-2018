5d4245d1fe72fa1cb04947ac9e2f7a045ac0c179
Hoyoung Jung <honux77@gmail.com>
2018-12-29 08:32:31 +0900

    change indent
    Change tab to space 4

--- src/main/java/net/honux/qna/QnaApplication.java
+++ src/main/java/net/honux/qna/QnaApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QnaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QnaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QnaApplication.class, args);
+    }
 
 }
 
